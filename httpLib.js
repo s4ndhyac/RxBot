b18c03f687b4f939caf79475a4fd22fbd4620727
Sandhya Chandramohan <s4ndhyac@gmail.com>
2019-07-15 22:29:13 -0700

    httpget json and xml fix

--- httpLib.js
+++ httpLib.js
@@ -1,7 +1,7 @@
 const https = require('https');
 var parseString = require('xml2js').parseString;
 
-exports.getRequest = function(req, callback) {
+exports.getRequestXML = function(req, callback) {
     https.get(req, (resp) => {
     let data = '';
 
@@ -15,6 +15,26 @@ exports.getRequest = function(req, callback) {
             parseString(data, function (err, result) {
             callback(result);
         });
+    });
+
+    }).on("error", (err) => {
+    console.log("Error: " + err.message);
+    });
+}
+
+
+exports.getRequestJson = function(req, callback) {
+    https.get(req, (resp) => {
+    let data = '';
+
+    // A chunk of data has been recieved.
+    resp.on('data', (chunk) => {
+        data += chunk;
+    });
+
+    // The whole response has been received. Print out the result.
+    resp.on('end', () => {
+            callback(data);
     });
 
     }).on("error", (err) => {
